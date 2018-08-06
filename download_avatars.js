19163d881dac8115711f9d617fc9a76be2876360
Chris <chrislo.7@live.com>
2018-08-06 20:32:15 +0000

    added comments

--- download_avatars.js
+++ download_avatars.js
@@ -1,15 +1,15 @@
 var request = require('request');
 var fs = require('fs');
 var token = require('./secrets.js');
-var owner = process.argv [2];
-var repo = process.argv [3];
+var owner = process.argv [2];                                         //repo owner will be entered after node download_avatars.js
+var repo = process.argv [3];                                          //repo name will be entered after node download_avatars.js <OWNER>
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
 
   var options = {
-    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors", //grabs url from
     headers: {
       'User-Agent': 'request',
       'Authorization': token.GITHUB_TOKEN
@@ -17,7 +17,7 @@ function getRepoContributors(repoOwner, repoName, cb) {
   };
 
   request(options, function(err, res, body) {
-    cb(JSON.parse(err), JSON.parse(body));
+    cb(JSON.parse(err), JSON.parse(body));                            // parses the error (in case there is one) and the body of the text
   });
 }
 
@@ -26,28 +26,24 @@ function downloadImageByURL(url, filePath) {
        .on('error', function (err) {
          throw err;
        })
-       .pipe(fs.createWriteStream(filePath));
+       .pipe(fs.createWriteStream(filePath));                         // creates a writable stream with filepath
 }
 
-
-
-
-
-getRepoContributors(owner, repo, function(err, result) {
-  if (!owner || !repo) {
-    console.log("Aye i need both dem owner and repo names")
+getRepoContributors(owner, repo, function(err, result) {              //execution
+  if (!owner || !repo) {                                              //if user does not enter a owner name or repo name, it will not process the request to download
+    console.log("Aye i need both dem owner and repo names");
+    console.log("Please enter in this format: $node download_avatars.js <REPOOWNER> <REPONAME>")
   } else {
-
-      console.log("Errors: ", err);
-  for (var i of result){                                              //using for-of loop to grab all IDs
-    console.log("Avatar ID: " +i.login+ ", URL: ", i.avatar_url);
-  }
-  console.log('Downloading images...')
-  for (var x of result){                                              //using for-of loop to grab url and create file name based on avatarname
-    var img = x.login;
-    downloadImageByURL(x.avatar_url, './avatars/'+x.login+'.jpg');
-  }
-  console.log('Download Complete. Have fun.')
+    console.log("Errors: ", err);
+    for (var i of result){                                              //using for-of loop to grab all IDs
+      console.log("Avatar ID: " + i.login + ", URL: ", i.avatar_url);
+    }
+    console.log('Downloading images...')
+    for (var x of result){                                              //using for-of loop to grab url and create file name based on avatarname
+      var img = x.login;
+      downloadImageByURL(x.avatar_url, './avatars/' + x.login + '.jpg');
+    }
+    console.log('Download Complete. Have fun.')
   }
 
 });
