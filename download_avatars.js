d18dce1aa34e3f2afdb6c64630baaf0a5d8e2e08
Chris <chrislo.7@live.com>
2018-08-06 22:02:33 +0000

    test

--- download_avatars.js
+++ download_avatars.js
@@ -1,10 +1,11 @@
-require('dotenv').config()
+require("dotenv").config();
 
 var request = require("request");
 var fs = require("fs");
 var token = require("./secrets.js");
 var owner = process.argv[2]; //repo owner will be entered after node download_avatars.js
 var repo = process.argv[3]; //repo name will be entered after node download_avatars.js <OWNER>
+var dir = "./avatars/"; // directory to store avatar images
 
 console.log("Welcome to the GitHub Avatar Downloader!");
 
@@ -18,7 +19,7 @@ function getRepoContributors(repoOwner, repoName, cb) {
       "/contributors", //grabs url from
     headers: {
       "User-Agent": "request",
-      Authorization: process.env.GITHUB_TOKEN     // uses .env to avoid storing sensitive info
+      Authorization: process.env.GITHUB_TOKEN // uses .env to avoid storing sensitive info
     }
   };
 
@@ -38,24 +39,31 @@ function downloadImageByURL(url, filePath) {
 
 getRepoContributors(owner, repo, function(err, result) {
   //execution
+
   if (!owner || !repo) {
     //if user does not enter a owner name or repo name, it will not process the request to download
     console.log("Aye i need both dem owner and repo names");
     console.log(
       "Please enter in this format: $node download_avatars.js <REPOOWNER> <REPONAME>"
     );
-  } else {
-    console.log("Errors: ", err);
-    for (var i of result) {
-      //using for-of loop to grab all IDs
-      console.log("Avatar ID: " + i.login + ", URL: ", i.avatar_url);
-    }
-    console.log("Downloading images...");
-    for (var x of result) {
-      //using for-of loop to grab url and create file name based on avatarname
-      var img = x.login;
-      downloadImageByURL(x.avatar_url, "./avatars/" + x.login + ".jpg");
-    }
-    console.log("Download Complete. Have fun.");
   }
+  console.log("Errors: ", err);
+  if (!fs.existsSync(dir)) {
+    // checks if there is a ./avatars/ directory
+    console.log("ERROR! NO AVATAR DIRECTORY");
+    console.log("CREATING ./AVATARS/ DIRECTORY");
+    fs.mkdirSync(dir);
+    // creates ./avatars/ directory for user and continues with code
+  }
+  for (var i of result) {
+    //using for-of loop to grab all IDs
+    console.log("Avatar ID: " + i.login + ", URL: ", i.avatar_url);
+  }
+  console.log("Downloading images to ./avatars/");
+  for (var x of result) {
+    //using for-of loop to grab url and create file name based on avatarname
+    var img = x.login;
+    downloadImageByURL(x.avatar_url, dir + x.login + ".jpg");
+  }
+  console.log("Download Complete. Have fun.");
 });
