ff1a9140ac5027dfd2449369f2f2e3b6b6fbd51a
Chris <chrislo.7@live.com>
2018-08-06 19:02:05 +0000

    modified download_avatars.js

--- download_avatars.js
+++ download_avatars.js
@@ -13,12 +13,19 @@ function getRepoContributors(repoOwner, repoName, cb) {
   };
 
   request(options, function(err, res, body) {
-    cb(err, body);
+    cb(JSON.parse(err), JSON.parse(body));
   });
 }
 
 
 getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
+  console.log("Errors: ", err);
+  for (var i of result){
+    console.log("Results: ", i.avatar_url);
+  }
 });
+
+
+
+
+
