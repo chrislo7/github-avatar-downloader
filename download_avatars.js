535a49e4607f6db2f000da7e884cb2ac30708ebe
Chris <chrislo.7@live.com>
2018-08-06 19:58:22 +0000

    added function to download avatar by URL

--- download_avatars.js
+++ download_avatars.js
@@ -1,4 +1,5 @@
 var request = require('request');
+var fs = require('fs');
 var token = require('./secrets.js');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
@@ -17,13 +18,29 @@ function getRepoContributors(repoOwner, repoName, cb) {
   });
 }
 
+function downloadImageByURL(url, filePath) {
+  request.get(url)
+       .on('error', function (err) {
+         throw err;
+       })
+       .pipe(fs.createWriteStream(filePath));
+}
+
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors: ", err);
   for (var i of result){
-    console.log("Results: ", i.avatar_url);
+    console.log("Avatar ID: " +i.login+ ", URL: ", i.avatar_url);
   }
+  console.log('Downloading image...')
+  for (var x of result){
+    var img = x.login;
+    downloadImageByURL(x.avatar_url, './avatars/'+x.login+'.jpg');
+  }
+  console.log('Download Complete.')
 });
+
+
 
 
 
